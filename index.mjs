75db5e618bdf2156ef9c8bfcacfec940e0c3a65f
JoseAlmendra <54294829+JoseAlmendra@users.noreply.github.com>
2025-04-18 21:39:54 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -80,10 +80,11 @@ async function scrapeOrderDetails(url) {
           totalPrice = Array.from(totalPriceContainer.children)
             .map(span => span.innerText)
             .join('');
+          totalPrice = totalPrice.replace('MX$', '').trim(); // Elimina "MX$" y espacios
           console.log('LOG: totalPrice extraído (con spans):', totalPrice);
         } else {
           const totalPriceElement = document.querySelector('.order-price .order-price-item.bold-font .rightPriceClass');
-          totalPrice = totalPriceElement?.innerText?.trim() || 'No total price found';
+          totalPrice = totalPriceElement?.innerText?.replace('MX$', '').trim() || 'No total price found'; // Elimina "MX$" y espacios
           console.log('LOG: totalPrice extraído (sin spans):', totalPrice);
         }
 
